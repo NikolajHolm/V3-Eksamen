7a4c28c52d8e6b7488d55f26d049880a0fa38b9f
NikolajHolm <niko4467@edu.campusvejle.dk>
2017-01-10 08:55:30 +0100

    Har lavet en smule af siden

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Islændere i DK</title>
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Google fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.css">
+</head>
+<body>
+	<header class="col-xs-12">
+		<h1 class="col-md-9 text-center">Islændere i DK</h1>
+		<div class="pull-right pull-top">
+			<img class ="logo img-responsive col-md-12"src="img/medlem-emblem.png" alt="">
+		</div>
+	</header>
+	<nav class="col-md-12">
+		<ul class="">
+			<li><a id="forside"href="#">Forside</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">Nyheder</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">Sitemap</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">Print</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">Kontakt</a></li>
+		</ul>
+				
+		<!-- Søgebar -->
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<div id="imaginary_container"> 
+					<div class="input-group stylish-input-group">
+						<input type="text" class="form-control"  placeholder="Search" >
+						<span class="input-group-addon">
+							<button class="searchicon" type="submit">
+								<span class="glyphicon glyphicon-search"></span>
+							</button>  
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Brugernavn -->
+		<div class="col-md-12">
+			<input type="text" name="firstname" placeholder="brugernavn">
+		</div>
+	</nav>
+	<main class="col-lg-11">
+		<article class ="">
+			<img src="img/articleImg1.jpg" alt="StormRider">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse a sapiente obcaecati! Facilis, perferendis similique, nobis quo distinctio possimus accusamus vel? Incidunt aliquam pariatur ratione nisi ullam quas id dolorum!</p>
+		</article>
+		<div class=" doc jumbotron col-md-6 text-center col-md-offset-3">
+			<i class="fa fa-book docs" aria-hidden="true"></i>
+			<p>Dokumenter</p>
+		</div>
+		<article >
+			<img src="img/articleImg2.jpg" alt="">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempore, dicta eligendi possimus. Placeat, illo, harum. Cumque, nulla culpa doloremque mollitia architecto nostrum obcaecati reprehenderit omnis, laudantium quisquam voluptatibus sed?</p>
+		</article>
+	</main>
+
+	<footer class="col-xs-12">
+		<a href="#">Islændere i DK</a>
+		<br>
+		<a href="#">Tågehornvej 32</a>
+		<p>9300 Ølstykke</p>
+		<p>islandere(at)live.com</p>
+	</footer>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+</html>
